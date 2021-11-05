63f892308f54bd8ad71f7d74fcbc298a3f02302d
mucerhq <18175731485@163.com>
2021-11-05 21:56:52 +0800

    增加了rest的表格生成

---
+++ GithubApi/src/main/java/POJO/Event/Root.java
@@ -0,0 +1,71 @@
+package POJO.Event;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Root {
+    private String id;
+
+    private String type;
+
+    private Actor actor;
+
+    private Repo repo;
+
+    private Payload payload;
+    @SerializedName("public")//和关键字重名 需要注解
+    private boolean ispublic;
+
+    private String created_at;
+
+    private Org org;
+
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public void setActor(Actor actor){
+        this.actor = actor;
+    }
+    public Actor getActor(){
+        return this.actor;
+    }
+    public void setRepo(Repo repo){
+        this.repo = repo;
+    }
+    public Repo getRepo(){
+        return this.repo;
+    }
+    public void setPayload(Payload payload){
+        this.payload = payload;
+    }
+    public Payload getPayload(){
+        return this.payload;
+    }
+    public void setPublic(boolean ispublic){
+        this.ispublic = ispublic;
+    }
+    public boolean getPublic(){
+        return this.ispublic;
+    }
+    public void setCreated_at(String created_at){
+        this.created_at = created_at;
+    }
+    public String getCreated_at(){
+        return this.created_at;
+    }
+    public void setOrg(Org org){
+        this.org = org;
+    }
+    public Org getOrg(){
+        return this.org;
+    }
+
+}
--- GithubApi/src/main/java/RESTCall.java
+++ GithubApi/src/main/java/RESTCall.java
@@ -1,25 +1,20 @@
+import POJO.Event.Root;
 import com.google.gson.Gson;
+import com.google.gson.*;
+import com.google.gson.internal.LinkedTreeMap;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
-class Commit{
-    Object sha;
-    Object node_id;
-    Object commit;
-    Object url;
-    Object html_url;
-    Object comments_url;
-    Object author;
-    Object committer;
-    Object parents;
-    Object stats;
-    Object files;
-}
 public class RESTCall {
+    ArrayList<LinkedTreeMap<String,Object>> table;
+    RESTCall(){
+        table= new ArrayList<LinkedTreeMap<String,Object>>();
+    }
     String makeRESTCall(String restUrl, String acceptHeaderValue)
             throws ClientProtocolException, IOException {
         Request request = Request.Get(restUrl);
@@ -30,7 +25,7 @@ public class RESTCall {
 
         Content content = request.execute().returnContent();
         String jsonString = content.asString();
-        System.out.println("content = " + jsonString);
+        //System.out.println("content = " + jsonString);
 
         // To print response JSON, using GSON. Any other JSON parser can be used here.
         //Map jsonMap = gson.fromJson(jsonString, Map.class);
@@ -39,17 +34,54 @@ public class RESTCall {
     String makeRESTCall(String restUrl) throws ClientProtocolException, IOException {
         return makeRESTCall(restUrl, null);
     }
-    void parseCommit(String jsonString){
+    void parseEvent(String jsonString){
+        //Json的解析类对象
+        JsonParser parser = new JsonParser();
+        //将JSON的String 转成一个JsonArray对象
+        JsonArray jsonArray = parser.parse(jsonString).getAsJsonArray();
         Gson gson = new Gson();
-        Map map=gson.fromJson(jsonString,Map.class);
-        //Commit commit = gson.fromJson(jsonString,Commit.class);
-        //String tString=gson.fromJson(jsonString,)
+        //对于数组形的json
+        for(JsonElement jsonElement:jsonArray){
+            POJO.Event.Root root = gson.fromJson(jsonElement , POJO.Event.Root.class);
+            LinkedTreeMap<String,Object> row= new LinkedTreeMap<String,Object>();
+            row.put("author",root.getActor().getLogin());
+            row.put("data",root.getCreated_at());
+            row.put("project",root.getRepo().getName());
+            if(root.getPayload().getCommits()!=null&& !root.getPayload().getCommits().isEmpty()){
+                String commitUrl=root.getPayload().getCommits().get(0).getUrl();
+                parseCommit(commitUrl,row);
+            }
+            table.add(row);
+        }
+    }
+    void parseCommit(String url,LinkedTreeMap<String,Object>row){
+        String jsonString = null;
+        try {
+            jsonString = makeRESTCall(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new Gson();
+        POJO.Commits.Root root=gson.fromJson(jsonString,POJO.Commits.Root.class);
+        row.put("addLines",root.getStats().getAdditions());
+        row.put("deleteLines",root.getStats().getDeletions());
+        row.put("totalLines",root.getStats().getTotal());
     }
     public static void main(String[] args) {
         RESTCall r= new RESTCall();
         try {
             String str= r.makeRESTCall("https://api.github.com/repos/Distributed-System-Course/Student-Grade-Accessment-System/events");
-            r.parseCommit(str);
+            r.parseEvent(str);
+            for (LinkedTreeMap<String,Object> row:r.table){
+                for (String key : row.keySet()){
+                    System.out.print(key+"\t\t\t\t\t\t");
+                }
+                System.out.println();
+                for (String key : row.keySet()){
+                    System.out.print(row.get(key)+"\t");
+                }
+                System.out.println();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
