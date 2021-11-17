7cbd6e938f181cc08b644942c081e2f73e3f15d4
Wu <3187053215@qq.com>
2021-11-18 00:02:54 +0800

    create_mine

---
+++ create_my_token/src/main/java/Application.java
@@ -0,0 +1,52 @@
+import com.google.gson.Gson;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+public class Application {
+    /**
+     * This method will make a REST GET call for this URL using Apache http client &
+     * fluent library.
+     *
+     * Then parse response using GSON & return parsed Map.
+     */
+    private static Map makeRESTCall(String restUrl, String acceptHeaderValue)
+            throws ClientProtocolException, IOException {
+        Request request = Request.Get(restUrl);
+        request.addHeader("User-Agent","Awesome-Octocat-App");//请求头 必须加User-Agent
+        if (acceptHeaderValue != null && !acceptHeaderValue.isBlank()) {
+            request.addHeader("Accept", acceptHeaderValue);
+        }
+
+        Content content = request.execute().returnContent();
+        String jsonString = content.asString();
+        // System.out.println("content = " + jsonString);
+        Gson gson=new Gson();
+        // To print response JSON, using GSON. Any other JSON parser can be used here.
+        Map jsonMap = gson.fromJson(jsonString, Map.class);
+        return jsonMap;
+    }
+
+    private static Map makeRESTCall(String restUrl) throws ClientProtocolException, IOException {
+        return makeRESTCall(restUrl, null);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Map res=Application.makeRESTCall("https://api.github.com/repos/Distributed-System-Course/Student-Grade-Accessment-System/commits"
+            );
+            Set<String> keys=res.keySet();
+            for (String key:keys){
+                Object value=res.get(key);
+                System.out.println(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
